f753345bfbb596b697452b35822bc9c947682cab
artsiom.vezhnavets <artsiom.vezhnavets@reconeyez.com>
2022-05-09 22:12:46 +0300

    Revert "Init"
    This reverts commit 7c8862c1

---
+++ src/kombinacii.kt
@@ -0,0 +1,53 @@
+
+class kombinacii() {
+    fun cards() {
+
+        val no = "ty chto-to pereputal"
+
+   when(karta.FR.opacity){
+       10 -> println(karta.FR.cardsName)
+      else -> println("$no")
+   }
+        when(karta.SF.opacity){
+            9-> println(karta.SF.cardsName)
+            else -> println("$no")
+        }
+        when(karta.KARE.opacity){
+           8 -> println(karta.KARE.cardsName)
+            else -> println("$no")
+        }
+        when(karta.FH.opacity){
+            7 -> println(karta.FH.cardsName)
+            else -> println("$no")
+        }
+        when(karta.FL.opacity){
+            6 -> println(karta.FL.cardsName)
+            else -> println("$no")
+        }
+        when(karta.ST.opacity){
+            5 -> println(karta.ST.cardsName)
+            else -> println("$no")
+        }
+        when(karta.SE.opacity){
+            4 -> println(karta.SE.cardsName)
+            else -> println("$no")
+        }
+        when(karta.DP.opacity){
+            3 -> println(karta.DP.cardsName)
+            else -> println("$no")
+        }
+        when(karta.PARA.opacity){
+            2 -> println(karta.PARA.cardsName)
+            else -> println("$no")
+        }
+        when(karta.SK.opacity){
+            1 -> println(karta.SK.cardsName)
+            else -> println("$no")
+        }
+
+    }
+}
+
+
+
+
--- src/mast.kt
+++ src/mast.kt
@@ -1,4 +1,4 @@
-enum class suits(val suits:String) {
+enum class mast(val suits:String) {
     CHIRWA("CHIRWA"),
     KRESTA("KRESTA"),
     BUBA("BUBA"),
